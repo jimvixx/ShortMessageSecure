a65712c9a46184740e92e4ee00813bcb07ae3880
Jimmy <86015747+jimvixx@users.noreply.github.com>
2026-06-11 00:34:00 +0300

    Align AndroidX Core with AGP 8.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -149,7 +149,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.core:core-ktx:1.19.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.fragment:fragment:1.8.9")
     implementation("androidx.gridlayout:gridlayout:1.1.0")
     implementation("androidx.legacy:legacy-support-v13:1.0.0")
